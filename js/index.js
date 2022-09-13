78e38c180c1593889c709b41ea94153eef611926
oyesa <mercyoyesa2018@gmail.com>
2022-09-13 14:21:03 +0300

    add JS code to landing page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+
+// Typing intro landing page
+var typed = new Typed(".auto-input", {
+  strings: ["Oyesa Oluchina", "  A Web <br> Developer."],
+  typeSpeed: 150,
+  backSpeed: 100,
+  loop: true
+})
+
+
 // show/hide skill items
 
 const skillItems = document.querySelectorAll('.skills .skill');
